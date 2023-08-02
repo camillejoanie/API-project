b63a24ce6eda99adcd8ea224d8b30503877365bf
camille <huangcamillej@gmail.com>
2023-08-02 13:56:10 -0700

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,9 +6,12 @@ router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
     res.status(200).json({
-      'XSRF-Token': csrfToken
+        'XSRF-Token': csrfToken
     });
 });
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 module.exports = router;
