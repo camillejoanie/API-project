8ac9c243d9b2dc915df85e4ede6f36f9b0544a0d
camille <huangcamillej@gmail.com>
2023-08-04 14:55:30 -0700

    Add firstName and lastName to seeders

--- backend/db/seeders/20230803210137-demo-user.js
+++ backend/db/seeders/20230803210137-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'jackson',
+        lastName: 'wang',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'camille',
+        lastName: 'huang',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'randy',
+        lastName: 'tom',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
