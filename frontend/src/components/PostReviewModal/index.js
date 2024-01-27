d29eb44dc95da6fccde57f1c6fe9b7a59623e1f1
camille <huangcamillej@gmail.com>
2024-01-26 16:02:26 -0800

    some css changes

--- frontend/src/components/PostReviewModal/index.js
+++ frontend/src/components/PostReviewModal/index.js
@@ -18,15 +18,6 @@ function PostReviewModal(props) {
   const [errors, setErrors] = useState({});
   const [isDisabled, setIsDisabled] = useState(true);
 
-  // let isDisabled = false;
-  // if (review.length < 10 || stars === "") {
-  //   isDisabled = true;
-  // }
-
-  // const reviewRating = (stars) => {
-  //   setStars(stars);
-  // };
-
   const updateDisabledState = () => {
     setIsDisabled(review.length < 10 || stars === "");
   };
@@ -79,7 +70,6 @@ function PostReviewModal(props) {
         placeholder="Leave your review here..."
         onChange={(e) => {
           setReview(e.target.value);
-          // Update disabled state when input changes
           updateDisabledState();
         }}
         value={review}
